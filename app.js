124724ae941528645e7896c756a5b8c355c22c9c
Niklas Haim <haim@Niklass-MacBook-Pro.local>
2020-10-29 11:22:55 +0100

    minor

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express'); const path = require("path");
 const PORT = process.env.PORT || 5000;
-const main = require("../routes/main.js");
 express()
     .use(express.static(path.join(__dirname, "public"))) 
     .use(express.static(path.join(__dirname, "public/HTML"))) 

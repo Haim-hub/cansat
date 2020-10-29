bc6de49ac002a272585aba0f1e120005248384c3
Niklas Haim <haim@Niklass-MacBook-Pro.local>
2020-10-29 11:17:58 +0100

    minor

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const express = require('express'); const path = require("path");
 const PORT = process.env.PORT || 5000;
 const main = require("../routes/main.js");
 express()
-    .use(express.static(path.join(__dirname, "public/HTML/index.html"))) 
-    .use("/main", main)
+    .use(express.static(path.join(__dirname, "public/HTML"))) 
     .listen(PORT, () => console.log(`Listening on ${PORT}`));

05e2e33f4698b6e552108ac56fbcb503b7e78709
Niklas Haim <haim@Niklass-MacBook-Pro.local>
2020-11-03 11:50:08 +0100

    minor

--- routes/data.js
+++ routes/data.js
@@ -33,7 +33,7 @@ router.get("/add", async (req, res) => { try {
     let alt = ((Math.log(pressure/101325.0)*8.31*(temp+273.15))/(284.430408))*(-1000);
     console.log(temp);
     console.log(pressure);
-    console.log(((Math.log(pressure/101325.0)*8.31*(temp+273.15))/(284.430408))*-1000);
+    console.log(((Math.log(pressure/101325.0)*8.31*(temp+273.15))/(284.430408))*(-1000));
     const SQL_query = {
         text: "INSERT INTO cansat (temp,pressure,alt) VALUES ($1, $2, $3)", 
         values: [temp, pressure, alt]

3d0623f8689f6dc73e0528886f7b2527ee6a6636
Niklas Haim <haim@Niklass-MacBook-Pro.local>
2020-11-12 12:47:36 +0100

    minor

--- routes/data.js
+++ routes/data.js
@@ -64,7 +64,7 @@ const secclient = await pool.connect(); // second query
 
 const result = await secclient.query("SELECT MAX(num) FROM cansat");
 
-console.log(result);
+console.log(result.rows);
 
 let newnum = result+1;
 

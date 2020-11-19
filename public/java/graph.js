bc977e24d9e94acdfaafd8a195572b6b8b353e0f
Niklas Haim <haim@Niklass-MacBook-Pro.local>
2020-11-19 12:20:27 +0100

    minor update

--- public/java/graph.js
+++ public/java/graph.js
@@ -87,9 +87,9 @@ function removeData(chart) {
 
 function xhrLoad() {
   let dbdata = JSON.parse(this.responseText).dbdata;
-  document.getElementById("temptext").innerHTML = dbdata[dbdata.length].temp;
-  document.getElementById("heighttext").innerHTML = dbdata[dbdata.length].alt;
-  document.getElementById("pressuretext").innerHTML = dbdata[dbdata.length].pressure;
+  document.getElementById("temptext").innerHTML = dbdata[parseInt(dbdata.length)].temp;
+  document.getElementById("heighttext").innerHTML = dbdata[parseInt(dbdata.length)].alt;
+  document.getElementById("pressuretext").innerHTML = dbdata[parseInt(dbdata.length)].pressure;
   for(let index = myLineChart.data.labels[myLineChart.data.labels.length-1]; index <= dbdata.length; index++) 
   {
     addData(myLineChart, dbdata[index].id, dbdata[index].temp, dbdata[index].pressure, dbdata[index].alt);
